7e8a81d01d54a2697427a649c180143de918f009
nspaskov <nikolay.spaskov@gmail.com>
2022-07-28 20:03:15 +0300

    Update vcd_org_saml.ps1

--- vcd_org_saml.ps1
+++ vcd_org_saml.ps1
@@ -1,17 +1,13 @@
 $creds = Get-Credential
-$vcd_instances = Get-Content ~/desktop/orgs_list
-$info = @()
+$vcd_instances = Get-Content Desktop/vcds
 foreach ($vcd in $vcd_instances) {
-    $server = Connect-CIServer -Server $vcd -NotDefault -Credential $creds
+        $server = Connect-CIServer -Server $vcd -NotDefault -Credential $creds
     $orgs = get-org -Server $server
     foreach ($org in $orgs) {
-        $data = "" | Select-Object org, federation, CertificateExpiration
-        $data.org = $org.name
-        $data.federation = $org.ExtensionData.Settings.OrgFederationSettings.Enabled
-        $data.CertificateExpiration = $org.ExtensionData.Settings.OrgFederationSettings.CertificateExpiration
-        $info += $data
-        $info
+    if ($Org.ExtensionData.Settings.OrgFederationSettings.Enabled -match "True") {
+        $Org.ExtensionData.Settings.OrgFederationSettings.RegenerateFederationCertificate()
+        Write-Host $org.name
+    }
     }
     Disconnect-CIServer -Server $server -Confirm:$false
 }
-$info | Export-Csv -Path "~/desktop/saml.csv"
